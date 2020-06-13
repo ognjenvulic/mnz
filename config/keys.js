e3d551b95fbf8dde22157a3bb854a98272e96b3e
ognjenvulic <ogi@goodwall.org>
2020-06-13 17:57:24 +0200

    add keys

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID: '705645926300-8nsnm5f6gn4rvih8u6nd241i3or68d8b.apps.googleusercontent.com',
+  googleClientSecret: 'sDg0pjH5Yeh4sLITpIyJg_dD',
+  //mongoURI: 'mongodb+srv://ogi:NijeBitno1@cluster0-gnzzp.gcp.mongodb.net/mnz-dev?retryWrites=true&w=majority',
+  mongoURI: 'mongodb+srv://mnz-dev:NijeBitnoDev1@cluster0-gnzzp.gcp.mongodb.net/mnz-prod?retryWrites=true&w=majority',
+  //mongoURI: 'mongodb+srv://mnz-prod:NijeBitnoProd1@cluster0-gnzzp.gcp.mongodb.net/mnz-prod?retryWrites=true&w=majority',
+  cookieKey: 'asdagsgseadhnsfghnbsfghsthsvbsdgf'
+}
