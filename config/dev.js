21df88f2bd61653aee02608edc6945e4296f3422
ognjenvulic <ogi@goodwall.org>
2020-06-13 20:27:25 +0200

    Setting up keys.

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID:  '705645926300-8nsnm5f6gn4rvih8u6nd241i3or68d8b.apps.googleusercontent.com',
+  googleClientSecret: 'sDg0pjH5Yeh4sLITpIyJg_dD',
+  mongoURI: 'mongodb+srv://mnz-dev:NijeBitnoDev1@cluster0-gnzzp.gcp.mongodb.net/mnz-dev?retryWrites=true&w=majority',
+  cookieKey: 'asdagsgseadhnsfghnbsfghsthsvbsdgf'
+};
