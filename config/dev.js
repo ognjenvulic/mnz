d4c872ab3284a98b6f10cf186a1e6f9dede44aa1
ognjenvulic <ogi@goodwall.org>
2020-06-13 20:43:57 +0200

    added another google auth account.

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,6 @@
 module.exports = {
-  googleClientID:  '705645926300-8nsnm5f6gn4rvih8u6nd241i3or68d8b.apps.googleusercontent.com',
-  googleClientSecret: 'sDg0pjH5Yeh4sLITpIyJg_dD',
+  googleClientID:  '705645926300-1u14h1nk0ejvqn6uu15le7cpb7b809sj.apps.googleusercontent.com',
+  googleClientSecret: 'LQcUnB0ixpRnuvdeOAHOPbGh',
   mongoURI: 'mongodb+srv://mnz-dev:NijeBitnoDev1@cluster0-gnzzp.gcp.mongodb.net/mnz-dev?retryWrites=true&w=majority',
   cookieKey: 'asdagsgseadhnsfghnbsfghsthsvbsdgf'
 };
